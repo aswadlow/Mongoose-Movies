f6f2d0eac5a1d438a09717c7b1c6c1b42fec97ea
Alanna Swadlow <aswadlow@gmail.com>
2023-12-05 09:41:22 -0800

    Initial commit

--- controllers/movies.js
+++ controllers/movies.js
@@ -18,6 +18,7 @@ async function show(req, res) {
 }
 
 function newMovie(req, res) {
+  
   // We'll want to be able to render an  
   // errorMsg if the create action fails
   res.render('movies/new', { title: 'Add Movie', errorMsg: '' });
